c52361ec70f8833dcae98b30bffc69221388d928
LT <qoo811103@gmail.com>
2025-08-28 00:14:06 +0800

    建立IOrderService

---
+++ interview/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using interview.Models;
+
+namespace interview.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> GetAllAsync();
+        Task<Order?> GetByIdAsync(int id);
+        Task<bool> UpdateAsync(int id, Order order);
+        Task<Order> AddAsync(Order order);
+        Task<bool> DeleteAsync(int id);
+    }
+}
